662e5b703f976642bd729c8b9fc1fa04ba3f984c
dantheking-crypto <danielfangzhu@gmail.com>
2022-06-13 21:14:51 -0700

    For block working graphically

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Blocks',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Blocks'),
+            ),
+            body: For()));
+  }
+}
+
+abstract class IStatement {
+  late Set<IStatement> body;
+  void run() {
+    for (var statement in body) {
+      statement.run();
+    }
+  }
+}
+
+class For extends StatelessWidget implements IStatement {
+  For({Key? key}) : super(key: key);
+  @override
+  var body = <IStatement>{};
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        width: 320,
+        height: 50,
+        decoration: const BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const <Widget>[
+                Text('for', style: TextStyle(color: Colors.white)),
+                Expanded(child: TextBox()),
+                Text('in range', style: TextStyle(color: Colors.white)),
+                Expanded(child: TextBox()),
+                Text('to', style: TextStyle(color: Colors.white)),
+                Expanded(child: TextBox()),
+              ],
+            )));
+  }
+
+  @override
+  void run() {}
+}
+
+class TextBox extends StatelessWidget {
+  const TextBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+        child: TextField(
+          decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Colors.white),
+        ));
+  }
+}
